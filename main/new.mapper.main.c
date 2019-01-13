74bd69ebe82f33298a535f3e2de83fe30a020bef
Lionishy <lionishy@gmail.com>
2019-01-13 10:59:08 +0300

    new-mapper: branches splitted to different files

--- main/new.mapper.main.c
+++ main/new.mapper.main.c
@@ -6,24 +6,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+enum WriteState {WAIT,WRITE};
 
 int main() {
     //ФИЗИЧЕСКАЯ МОДЕЛЬ
     VectorSp R0 = {1.975,0.44928364,0.}; //точка нормировки
-    VectorH V0  = {0.05,0.12247449};
+    VectorH V0  = {0.0,0.05};
 
     //параметры окружающей плазмы
     homogeneous_physical_environment_context_t physical_environment_cntx = {
           .R0 = R0, .V0 = V0
         , .omega_cc0 = 1., .omega_pc0 = 0.1
-        , .cold_density = 0.05, .source_density = 0.99
+        , .cold_density = 0.001, .source_density = 1.0
     };
 
-    FILE *map_file_up = fopen("./new_map_up1.txt","w");
-    FILE *map_file_dn = fopen("./new_map_dn1.txt","w");
-    double dw = 1.e-6;
+    
+    char name[] = "./new_map_branch_";
+    char ext[] = ".txt";
+    int branch_counter = 0;
+    enum WriteState st = WAIT; 
 
-    unsigned counter = 0;
+    FILE *map_file = NULL;
+    double dw = 0.00001;
+
     for (double w = 2.0; w > 0.; w -= dw) {
         VectorH K = {0.,0.};
         double res = warm_left_dispersion_relation(
@@ -33,28 +40,35 @@ int main() {
             );
 
         if (res > 0.) {
-            if(0 == (++counter)%10)
-                fprintf(map_file_up,"%f %f %f\n",w,sqrt(res),sqrt(res)*w);
+            if (WAIT == st) {
+                char count_name[3];
+                sprintf(count_name,"%d",++branch_counter);
+                char* full_name = malloc(strlen(name)+strlen(count_name)+strlen(ext)+1);
+                strcpy(full_name,name);
+                strcat(full_name,count_name);
+                strcat(full_name,ext);
+                printf("new branch: %s\n",full_name);
+
+                map_file = fopen(full_name,"w");
+                free(full_name);
+                if (NULL == map_file) {
+                    printf("Can't open file...");
+                    return 0;
+                }
+                st = WRITE;
+            }
+            fprintf(map_file,"%f %f %f\n",w,sqrt(res),sqrt(res)*w);
+        } else {
+            if (WRITE == st) {
+                fclose(map_file);
+                st = WAIT;
+            }
         }
-        else
-            printf("Negative %f %f\n",w,res);
+            
     }
 
-    /*for (double w = 1.+dw; w > 0.; w -= dw) {
-        VectorH K = {0.,0.};
-        double res = warm_left_dispersion_relation(
-            homogeneous_physical_environment(&physical_environment_cntx,R0)
-            , K
-            , w
-            );
-
-        if (res > 0.)
-            fprintf(map_file_dn,"%f %f %f\n",w,sqrt(res),sqrt(res)*w);
-    }*/
-
     END: {
-        if (NULL != map_file_dn) fclose(map_file_dn);
-        if (NULL != map_file_up) fclose(map_file_up);
+        if (NULL != map_file && WRITE == st) fclose(map_file);
     }
 
 }
